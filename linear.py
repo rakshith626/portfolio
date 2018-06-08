69228975e39372408e3cbce7a366607fdf0c6492
Thodupunoori Rakshith Guptha <rakshith1749@gmail.com>
2018-06-08 22:54:45 +0530

    linear search updated

---
+++ linear.py
@@ -0,0 +1,10 @@
+listplayers = ['hardik', 'krunal', 'lewis', 'rohith', 'Pollard', 'kishan', 'bumrah', 'cuttting']
+
+i=0
+l = len(listplayers)
+while i<l:
+  if listplayers[i] == "rohith":
+      print ("rohith found at index", i)
+      break;
+  else:
+      i=i+1
