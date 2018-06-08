66b6a5eec49661c9f4cda4f9e8ed30d9ee7da3e5
Thodupunoori Rakshith Guptha <rakshith1749@gmail.com>
2018-06-08 22:19:39 +0530

    printing sentence in python

---
+++ task2.py
@@ -0,0 +1,15 @@
+#TASK 2
+
+​
+
+print("“hey,hai \n")
+
+print("     i am glad to tell you that ,\n")
+
+print("              i am learning python :)”")
+
+“hey,hai 
+
+     i am glad to tell you that ,
+
+              i am learning python :)”
